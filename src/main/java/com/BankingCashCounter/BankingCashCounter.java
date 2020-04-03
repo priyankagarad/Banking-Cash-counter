672f069a8ae8bc5aaa44695253d48d57117f61a7
priyankagarad <garadpriyanka1995@gmail.com>
2020-04-03 11:57:18 +0530

    ADDED:welcome message

---
+++ src/main/java/com/BankingCashCounter/BankingCashCounter.java
@@ -0,0 +1,7 @@
+package com.BankingCashCounter;
+public class BankingCashCounter {
+    public static void main(String args[])
+    {
+        System.out.println("welcome to Banking Cash Counter");
+    }
+}
